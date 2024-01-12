Added angular offset in sources. Fixed averaging for non-uniform weight beams.

abel/classes/beam.py
@@ -5,7 +5,7 @@ from pytz import timezone
 from abel import CONFIG
 import scipy.constants as SI
 from abel.utilities.relativity import energy2proper_velocity, proper_velocity2energy, momentum2proper_velocity, proper_velocity2momentum, proper_velocity2gamma, energy2gamma, gamma2proper_velocity
-from abel.utilities.statistics import prct_clean, prct_clean2d
+from abel.utilities.statistics import weighted_mean, weighted_std, weighted_cov
 from abel.utilities.plasma_physics import k_p, wave_breaking_field, beta_matched
 from abel.physics_models.hills_equation import evolve_hills_equation_analytic
 from abel.physics_models.betatron_motion import evolve_betatron_motion
@@ -163,6 +163,9 @@ class Beam():
     # copy another beam's macroparticle charge
     def copy_particle_charge(self, beam):
         self.set_qs(np.median(beam.qs()))
+
+    def scale_charge(self, Q):
+        self.set_qs(Q*self.qs()/self.charge())
     
     
     def rs(self):
@@ -220,8 +223,7 @@ class Beam():
     ## BEAM STATISTICS
     
     def charge(self):
-        qs = self.qs()
-        return np.sum(qs[~np.isnan(qs)])
+        return np.nansum(self.qs())
     
     def abs_charge(self):
         return abs(self.charge())
@@ -230,108 +232,99 @@ class Beam():
         return self.charge()/abs(self.charge())
     
     def energy(self, clean=False):
-        return np.mean(prct_clean(self.Es(), clean))
+        return weighted_mean(self.Es(), self.qs(), clean)
     
     def gamma(self, clean=False):
-        return np.mean(prct_clean(self.gammas(), clean))
+        return weighted_mean(self.gammas(), self.qs(), clean)
     
     def total_energy(self):
-        return self.abs_charge()*self.energy()
+        return abs(np.nansum(self.qs()*self.Es()))
     
     def energy_spread(self, clean=False):
-        return np.std(prct_clean(self.Es(), clean))
+        return weighted_std(self.Es(), self.qs(), clean)
     
     def rel_energy_spread(self, clean=False):
         return self.energy_spread(clean)/self.energy(clean)
     
     def z_offset(self, clean=False):
-        return np.mean(prct_clean(self.zs(), clean))
+        return weighted_mean(self.zs(), self.qs(), clean)
     
     def bunch_length(self, clean=False):
-        return np.std(prct_clean(self.zs(), clean))
+        return weighted_std(self.zs(), self.qs(), clean)
     
     def x_offset(self, clean=False):
-        return np.mean(prct_clean(self.xs(), clean))
-    
-    def y_offset(self, clean=False):
-        return np.mean(prct_clean(self.ys(), clean))
-
-    def x_angle(self, clean=False):
-        return np.mean(prct_clean(self.xps(), clean))
-    
-    def y_angle(self, clean=False):
-        return np.mean(prct_clean(self.yps(), clean))
-        
-    def ux_offset(self, clean=False):
-        return np.mean(prct_clean(self.uxs(), clean))
-    
-    def uy_offset(self, clean=False):
-        return np.mean(prct_clean(self.uys(), clean))
+        return weighted_mean(self.xs(), self.qs(), clean)
     
     def beam_size_x(self, clean=False):
-        return np.std(prct_clean(self.xs(), clean))
+        return weighted_std(self.xs(), self.qs(), clean)
+
+    def y_offset(self, clean=False):
+        return weighted_mean(self.ys(), self.qs(), clean)
 
     def beam_size_y(self, clean=False):
-        return np.std(prct_clean(self.ys(), clean))
+        return weighted_std(self.ys(), self.qs(), clean)
+    
+    def x_angle(self, clean=False):
+        return weighted_mean(self.xps(), self.qs(), clean)
     
     def divergence_x(self, clean=False):
-        return np.std(prct_clean(self.xps(), clean))
+        return weighted_std(self.xps(), self.qs(), clean)
 
+    def y_angle(self, clean=False):
+        return weighted_mean(self.yps(), self.qs(), clean)
+    
     def divergence_y(self, clean=False):
-        return np.std(prct_clean(self.yps(), clean))
+        return weighted_std(self.yps(), self.qs(), clean)
+    
+    def ux_offset(self, clean=False):
+        return weighted_mean(self.uxs(), self.qs(), clean)
+    
+    def uy_offset(self, clean=False):
+        return weighted_mean(self.uys(), self.qs(), clean)
+
     
     def geom_emittance_x(self, clean=False):
-        xs, xps = prct_clean2d(self.xs(), self.xps(), clean)
-        return np.sqrt(np.linalg.det(np.cov(xs, xps)))
+        return np.sqrt(np.linalg.det(weighted_cov(self.xs(), self.xps(), self.qs(), clean)))
     
     def geom_emittance_y(self, clean=False):
-        ys, yps = prct_clean2d(self.ys(), self.yps(), clean)
-        return np.sqrt(np.linalg.det(np.cov(ys, yps)))
+        return np.sqrt(np.linalg.det(weighted_cov(self.ys(), self.yps(), self.qs(), clean)))
     
     def norm_emittance_x(self, clean=False):
-        xs, uxs = prct_clean2d(self.xs(), self.uxs(), clean)
-        return np.sqrt(np.linalg.det(np.cov(xs, uxs/SI.c)))
+        return np.sqrt(np.linalg.det(weighted_cov(self.xs(), self.uxs()/SI.c, self.qs(), clean)))
     
     def norm_emittance_y(self, clean=False):
-        ys, uys = prct_clean2d(self.ys(), self.uys(), clean)
-        return np.sqrt(np.linalg.det(np.cov(ys, uys/SI.c)))
+        return np.sqrt(np.linalg.det(weighted_cov(self.ys(), self.uys()/SI.c, self.qs(), clean)))
     
     def beta_x(self, clean=False):
-        xs, xps = prct_clean2d(self.xs(), self.xps(), clean)
-        covx = np.cov(xs, xps)
+        covx = weighted_cov(self.xs(), self.xps(), self.qs(), clean)
         return covx[0,0]/np.sqrt(np.linalg.det(covx))
     
     def beta_y(self, clean=False):
-        ys, yps = prct_clean2d(self.ys(), self.yps(), clean)
-        covy = np.cov(ys, yps)
+        covy = weighted_cov(self.ys(), self.yps(), self.qs(), clean)
         return covy[0,0]/np.sqrt(np.linalg.det(covy))
     
     def alpha_x(self, clean=False):
-        xs, xps = prct_clean2d(self.xs(), self.xps(), clean)
-        covx = np.cov(xs, xps)
+        covx = weighted_cov(self.xs(), self.xps(), self.qs(), clean)
         return -covx[1,0]/np.sqrt(np.linalg.det(covx))
     
     def alpha_y(self, clean=False):
-        ys, yps = prct_clean2d(self.ys(), self.yps(), clean)
-        covy = np.cov(ys, yps)
+        covy = weighted_cov(self.ys(), self.yps(), self.qs(), clean)
         return -covy[1,0]/np.sqrt(np.linalg.det(covy))
     
     def gamma_x(self, clean=False):
-        xs, xps = prct_clean2d(self.xs(), self.xps(), clean)
-        covx = np.cov(xs, xps)
+        covx = weighted_cov(self.xs(), self.xps(), self.qs(), clean)
         return covx[1,1]/np.sqrt(np.linalg.det(covx))
     
     def gamma_y(self, clean=False):
-        ys, yps = prct_clean2d(self.ys(), self.yps(), clean)
-        covy = np.cov(ys, yps)
+        covy = weighted_cov(self.ys(), self.yps(), self.qs(), clean)
         return covy[1,1]/np.sqrt(np.linalg.det(covy))
 
     def intrinsic_emittance(self):
-        covxy = np.cov(self.norm_transverse_vector())
+        covxy = np.cov(self.norm_transverse_vector(), aweights=abs(self.qs()))
         return np.sqrt(np.sqrt(np.linalg.det(covxy)))
 
     def angular_momentum(self):
-        covxy = np.cov(self.norm_transverse_vector())
+        covxy = np.cov(self.norm_transverse_vector(), aweights=abs(self.qs()))
         det_covxy_cross = np.linalg.det(covxy[2:4,0:2])
         return np.sign(covxy[3,0]-covxy[2,1])*np.sqrt(np.abs(det_covxy_cross))
 
@@ -341,25 +334,23 @@ class Beam():
     def eigen_emittance_min(self):
         return np.sqrt(self.norm_emittance_x()*self.norm_emittance_y()) - self.angular_momentum()
 
-    def norm_amplitude_x(self, n0=None, clean=False):
-        xs, xps = prct_clean2d(self.xs(), self.xps(), clean)
-        if n0 is not None:
-            beta_x = beta_matched(n0, self.energy())
+    def norm_amplitude_x(self, plasma_density=None, clean=False):
+        if plasma_density is not None:
+            beta_x = beta_matched(plasma_density, self.energy())
             alpha_x = 0
         else:
-            covx = np.cov(xs, xps)
+            covx = weighted_cov(self.xs(), self.xps(), self.qs(), clean)
             emgx = np.sqrt(np.linalg.det(covx))
             beta_x = covx[0,0]/emgx
             alpha_x = -covx[1,0]/emgx
         return np.sqrt(self.gamma()/beta_x)*np.sqrt(self.x_offset()**2 + (self.x_offset()*alpha_x + self.x_angle()*beta_x)**2)
         
-    def norm_amplitude_y(self, n0=None, clean=False):
-        ys, yps = prct_clean2d(self.ys(), self.yps(), clean)
-        if n0 is not None:
-            beta_y = beta_matched(n0, self.energy())
+    def norm_amplitude_y(self, plasma_density=None, clean=False):
+        if plasma_density is not None:
+            beta_y = beta_matched(plasma_density, self.energy())
             alpha_y = 0
         else:
-            covy = np.cov(ys, yps)
+            covy = weighted_cov(self.ys(), self.yps(), self.qs(), clean)
             emgy = np.sqrt(np.linalg.det(covy))
             beta_y = covy[0,0]/emgy
             alpha_y = -covy[1,0]/emgy

abel/classes/source/impl/source_basic.py
@@ -8,7 +8,7 @@ from abel.utilities.relativity import energy2gamma
 
 class SourceBasic(Source):
     
-    def __init__(self, length=0, num_particles=1000, energy=None, charge=0, rel_energy_spread=None, energy_spread=None, bunch_length=None, z_offset=0, x_offset=0, y_offset=0, emit_nx=0, emit_ny=0, beta_x=None, beta_y=None, alpha_x=0, alpha_y=0, angular_momentum=0, wallplug_efficiency=1, accel_gradient=None, symmetrize=False):
+    def __init__(self, length=0, num_particles=1000, energy=None, charge=0, rel_energy_spread=None, energy_spread=None, bunch_length=None, z_offset=0, x_offset=0, y_offset=0, x_angle=0, y_angle=0, emit_nx=0, emit_ny=0, beta_x=None, beta_y=None, alpha_x=0, alpha_y=0, angular_momentum=0, wallplug_efficiency=1, accel_gradient=None, symmetrize=False):
         self.energy = energy
         self.charge = charge
         self.rel_energy_spread = rel_energy_spread # [eV]
@@ -17,6 +17,8 @@ class SourceBasic(Source):
         self.z_offset = z_offset # [m]
         self.x_offset = x_offset # [m]
         self.y_offset = y_offset # [m]
+        self.x_angle = x_angle # [rad]
+        self.y_angle = y_angle # [rad]
         self.num_particles = num_particles
         self.emit_nx = emit_nx # [m rad]
         self.emit_ny = emit_ny # [m rad]
@@ -33,6 +35,8 @@ class SourceBasic(Source):
         self.jitter = SimpleNamespace()
         self.jitter.x = 0
         self.jitter.y = 0
+        self.jitter.xp = 0
+        self.jitter.yp = 0
         self.jitter.z = 0
         self.jitter.t = 0
         
@@ -53,6 +57,12 @@ class SourceBasic(Source):
         y0 = np.random.normal(scale=self.jitter.y) + self.y_offset
         xs += x0
         ys += y0
+
+        # add transverse jitters and offsets
+        xp0 = np.random.normal(scale=self.jitter.xp) + self.x_angle
+        yp0 = np.random.normal(scale=self.jitter.yp) + self.y_angle
+        xps += xp0
+        yps += yp0
         
         # generate relative/absolute energy spreads
         if self.rel_energy_spread is not None: