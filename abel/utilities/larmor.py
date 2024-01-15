6c80bc73cf70bdd9895f70837806b272c3f161a0
Daniel Kalvik <dkalvik@gmail.com>
2024-01-15 17:50:04 +0200

    Made lots of plots

--- abel/utilities/larmor.py
+++ abel/utilities/larmor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.constants as SI
+import abel.utilities.plasma_physics as pp
 
 const = SI.e**2 / 6 / np.pi / SI.epsilon_0 / SI.c
 
@@ -14,11 +15,29 @@ def larmor_formula(s, xp, yp, gammas):
     for i in range(n-1):
         gamma[i] = 1/2 * (gammas[i+1] + gammas[i])
         z[i] = 1/2 * (s[i+1] + s[i])
-        
         d_beta_x[i] = (xp[i+1] - xp[i])/dz
         d_beta_y[i] = (yp[i+1] - yp[i])/dz
 
-    P = 1/SI.c * const * np.power(gamma, 4) * SI.c**2 * (np.power(d_beta_x, 2) + np.power(d_beta_y, 2)) # [J/m]
+    P = 1/SI.c * const * np.power(gamma, 4) * SI.c**2 * (np.power(d_beta_x, 2) + np.power(d_beta_y, 2)) # [J/m] 1/c transforms 1/s to 1/m
 
     return z, P
-        
+
+def mean_larmor_formula(s, n0, gammas, rx, ry):
+    gamma0 = gammas[0]
+    kp = pp.k_p(n0)
+    k_beta = kp/np.sqrt(2*gammas)
+    const = SI.e**2 * SI.c / 12 / np.pi / SI.epsilon_0
+    P = 1/SI.c * const * np.power(gammas,4) * np.power(k_beta,4) * rx**2 + ry**2) # [J/m]
+
+    return s, P
+
+
+
+
+
+
+
+
+
+
+    
