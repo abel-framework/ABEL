b25d717e595bd2f7442c80a9eca0ec0f5dae9e22
Daniel Kalvik <dkalvik@gmail.com>
2024-04-22 17:24:42 +0300

    Added on the larmor utility and changed init.py to import hipace

--- abel/__init__.py
+++ abel/__init__.py
@@ -34,7 +34,7 @@ from .classes.source.impl.source_combiner import *
 from .classes.source.impl.source_from_file import *
 from .classes.stage.impl.stage_basic import *
 from .classes.stage.impl.stage_nonlinear_1d import *
-#from .classes.stage.impl.stage_hipace import *
+from .classes.stage.impl.stage_hipace import *
 from .classes.stage.impl.stage_wake_t import *
 from .classes.stage.impl.stage_quasistatic_2d import *
 from .classes.stage.impl.stage_slice_transverse_wake_instability import *
