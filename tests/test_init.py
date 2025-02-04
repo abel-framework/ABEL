1ad395e39e4127b96baccffbece4b4b1a5e734ce
Kyrre Ness Sjobak <k.n.sjobak@fys.uio.no>
2025-02-04 14:06:36 +0100

    Add pytest

---
+++ tests/test_init.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+MadXTools : TableFS Tests
+=========================
+
+This file is a part of MadXTools
+Copyright 2022– C.A.Lindstrøm, B.Chen, O.G. Finnerud,
+D. Kallvik, E. Hørlyk, K.N. Sjobak, E.Adli, University of Oslo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+import pytest
+
+@pytest.mark.core
+def testCore_init():
+    """Test package initialisation.
+    """
+    import abel
+
+# END Test testCore_init
