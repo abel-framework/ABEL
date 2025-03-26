47282f66dfcad74249e087b2a0447dd9aa16acea
Felipe Pena <felipe.pena@fys.uio.no>
2025-03-26 19:43:49 +0200

    Added the SourceEnergyComb in the __init__ file.

--- abel/__init__.py
+++ abel/__init__.py
@@ -42,6 +42,7 @@ from .classes.collider.collider import Collider
 from .classes.source.impl.source_basic import *
 from .classes.source.impl.source_trapezoid import SourceTrapezoid
 from .classes.source.impl.source_combiner import *
+from .classes.source.impl.source_energy_comb import *
 from .classes.source.impl.source_from_file import *
 from .classes.source.impl.source_flattop import *
 from .classes.stage.impl.stage_basic import *
