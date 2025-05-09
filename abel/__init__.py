b7232d2af9675be12790a66216e5d9d38a8bc79f
Knallvik <dkalvik@gmail.com>
2025-05-09 05:29:47 +0300

    added the source from profile in the init file

--- abel/__init__.py
+++ abel/__init__.py
@@ -44,6 +44,7 @@ from .classes.source.impl.source_basic import *
 from .classes.source.impl.source_trapezoid import SourceTrapezoid
 from .classes.source.impl.source_combiner import *
 from .classes.source.impl.source_from_file import *
+from .classes.source.impl.source_from_profile import *
 from .classes.source.impl.source_flattop import *
 from .classes.source.impl.source_capsule import *
 from .classes.stage.impl.stage_basic import *
