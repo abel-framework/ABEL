7e9493e5dbcedef85d35f7c8fa9535056d99f336
Knallvik <dkalvik@gmail.com>
2025-05-10 02:37:00 +0300

    moved source/stage-creation in assemble-trackables to __init__()

--- abel/classes/beamline/impl/experiment/impl/experiment_flashforward.py
+++ abel/classes/beamline/impl/experiment/impl/experiment_flashforward.py
@@ -1,7 +1,9 @@
 from abel.classes.beamline.impl.experiment.experiment import Experiment
 from abel.classes.source.impl.source_basic import SourceBasic
+from abel.classes.source.source import Source
 from abel.classes.stage.impl.stage_basic import StageBasic
 from abel.classes.spectrometer.impl.spectrometer_flashforward_impactx import SpectrometerFLASHForwardImpactX
+from abel.classes.beamline.impl.linac.linac import Linac
 
 class ExperimentFLASHForward(Experiment):
     
@@ -14,18 +16,8 @@ class ExperimentFLASHForward(Experiment):
         
         # set up empty elements
         source = SourceBasic()
-        stage = StageBasic()
-        spectrometer = SpectrometerFLASHForwardImpactX()
-        
-        super().__init__(linac=source, component=stage, spectrometer=spectrometer)
-        
-    # assemble the trackables
-    def assemble_trackables(self):
-
-        # define source
-        source = self.linac
         source.energy = self.energy
-        source.charge = 1e-9
+        source.charge = -0.75e-9
         source.rel_energy_spread = self.rel_energy_spread
         source.bunch_length = 150e-6
         source.z_offset = 0
@@ -34,16 +26,27 @@ class ExperimentFLASHForward(Experiment):
         source.beta_x = 0.02 # [m]
         source.beta_y = 0.02 # [m]
         source.num_particles = 100000
-
-        # define stage
-        stage = self.component
-        stage.length = self.plasma_length # [eV]
+        
+        stage = StageBasic()
+        stage.length = plasma_length # [eV]
         stage.nom_accel_gradient = 2e9 # [V/m]
         stage.plasma_density = 1e22 # [m^-3]
-
-        # define spectrometer
-        spectrometer = self.spectrometer
-        spectrometer.imaging_energy = source.energy
         
+        spectrometer = SpectrometerFLASHForwardImpactX()
+        spectrometer.imaging_energy_x = self.energy
+        spectrometer.imaging_energy_y = self.energy
+        
+        super().__init__(linac=source, component=stage, spectrometer=spectrometer)
+        
+    # assemble the trackables
+    def assemble_trackables(self):
+        # define spectrometer
+        if isinstance(self.linac, Source):
+            self.spectrometer.imaging_energy = self.linac.energy
+        elif isinstance(self.linac, Linac):
+            self.spectrometer.imaging_energy = self.linac.source.energy
+        else:
+            raise ValueError("You must define a linac that is either a type Source or a type Linac")
+
         # set the bunch train pattern etc.
         super().assemble_trackables()
