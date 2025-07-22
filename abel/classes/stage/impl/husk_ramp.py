a2596ffaf754c855d89c280102a0b5f7d1d0513f
Ben Chen <ben_chen93@hotmail.com>
2025-07-22 22:12:12 +0200

    Added the option to set nominal energy gain in HuskRamp.__init__(). Also added HuskRamp.print_summary().

--- abel/classes/stage/impl/husk_ramp.py
+++ abel/classes/stage/impl/husk_ramp.py
@@ -8,12 +8,17 @@ from abel.classes.stage.stage import Stage, StageError
 class HuskRamp(Stage):
     
     # ==================================================
-    def __init__(self, ramp_plasma_density=None, ramp_length=None, ramp_shape='uniform'):
+    def __init__(self, nom_energy_gain=None, ramp_plasma_density=None, ramp_length=None, ramp_shape='uniform'):
 
-        super().__init__(nom_accel_gradient=None, nom_energy_gain=None, plasma_density=ramp_plasma_density, driver_source=None, ramp_beta_mag=1.0)
+        super().__init__(nom_accel_gradient=None, nom_energy_gain=nom_energy_gain, plasma_density=ramp_plasma_density, driver_source=None, ramp_beta_mag=1.0)
+
+        # TODO: Need code to handle ramp_shape='from_file' ... need to ignore ramp_length, ramp_plasma_density. These need to be calculated.
 
         self.ramp_shape = ramp_shape
         self.length = ramp_length
+
+        # Acceleration gradient and length cannot be set until the ramp nominal 
+        # energy is known. These calculation are done by Stage._prepare_ramps().
 
 
     # ==================================================
@@ -29,5 +34,17 @@ class HuskRamp(Stage):
     # ==================================================
     def set_downramp(self):
         raise StageError('Cannot add a ramp to StageRamp.')
+    
+
+    # ==================================================
+    def print_summary(self):
+        if self.is_upramp():
+            print('Ramp type: \t\t\t\t\t\t upramp')
+        if self.is_downramp():
+            print('Ramp type: \t\t\t\t\t\t downramp')
+        print('Ramp shape: \t\t\t\t\t\t', self.ramp_shape)
+        super().print_summary()
+        
+        
     
 
