5457c52d7e17a402ca3426ae825a27e290c23709
Daniel Kalvik <dkalvik@gmail.com>
2025-08-21 15:33:02 +0200

    made a new driver delay system with more dipole-degrees of freedom

--- abel/classes/bds/impl/m_chicane_bds.py
+++ abel/classes/bds/impl/m_chicane_bds.py
@@ -1,17 +1,17 @@
 import numpy as np
 from abel.classes.bds.bds import BeamDeliverySystem
 import scipy.constants as SI
-from scipy.optimize import minimize
+from scipy.optimize import minimize, root_scalar, NonlinearConstraint
 
-class DriverDelaySystem(BeamDeliverySystem):
+class DriverDelaySystem_M(BeamDeliverySystem):
 
-    def __init__(self, E_nom=1, delay_per_stage=1, length_stage=10, num_stages=2, ks=[], \
-                 keep_data=False, enable_space_charge=False, enable_csr=False, enable_isr=False, \
-                 focus_at_trough = "x", fill_factor=0.7, mirrored=False, layoutnr = 1, use_monitors=False):
+    def __init__(self, E_nom=2, delay_per_stage=1, length_stage=12, num_stages=2, ks=[], B_dipole=1, \
+                 keep_data=False, enable_space_charge=False, enable_csr=False, enable_isr=False, layoutnr = 1,\
+                 l_diag = 2, use_monitors=False, x0=10):
         # E_nom in eV, delay in ns
         super().__init__()
         self._E_nom = E_nom #backing variable 
-        self._delay_per_stage = delay_per_stage/1e9
+        self._delay_per_stage = delay_per_stage*1e-9
         self.num_stages = num_stages
         self._length_stage = length_stage
         self.keep_data = keep_data
@@ -19,22 +19,18 @@ class DriverDelaySystem(BeamDeliverySystem):
         self.enable_csr = enable_csr
         self.enable_isr = enable_isr
         self.ks = ks
-        self.mirrored = mirrored
         self.layoutnr = layoutnr
         self.use_monitors = use_monitors
+        self.x0=x0
 
         # Lattice-elements lengths
         self.l_quads = 0.1
-        self.fill_factor = fill_factor
-        self.l_dipole, self.l_straight, self.l_drift = self.get_lattice_lengths(self.delay_per_stage)
+        self.l_kick = 2
+        self.l_gap = 0.25
+        self.l_diag = l_diag
         # 2*cell_length = L_tot = 2*L_stage+2*c*delay_per_stage
 
-        # Use lengths to get the required B-field
-        self.B_dipole = self.get_B_field(E_nom/1e9)
-
-        self.cell_length = self.get_length()
-
-        self.initial_focus_plane = focus_at_trough
+        self.B_dipole = B_dipole
 
     @property
     def E_nom(self):
@@ -51,35 +47,203 @@ class DriverDelaySystem(BeamDeliverySystem):
     @length_stage.setter
     def length_stage(self, value):
         self._length_stage = value
-        self.l_dipole, self.l_straight, self.l_drift = self.get_lattice_lengths(self.delay_per_stage/1e9)
-        self.cell_length = self.get_length()/4
-        self.B_dipole = self.get_B_field(self.E_nom/1e9)
 
     @delay_per_stage.setter
     def delay_per_stage(self, value):
-        self._delay_per_stage = value
-        self.l_dipole, self.l_straight, self.l_drift = self.get_lattice_lengths(value/1e9)
-        self.cell_length = self.get_length()/4
-        self.B_dipole=self.get_B_field(self.E_nom/1e9)
+        self._delay_per_stage = value*1e-9
 
 
     # Setting the new E_nom
+    
     @E_nom.setter
     def E_nom(self, value):
         self._E_nom = value
-        # Recalculate B if new energy is set
-        print("Recalculating B-field")
-        self.B_dipole=self.get_B_field(value/1e9)
 
-    def get_B_field(self, p):
-        # p in GeV, B given in Tesla
-        func = lambda B: 0 #### To be implemented ####
+    
+
+    def get_dipole_lengths(self):
+        """
+        Sets the lengths of the dipoles such that the delay is achieved
+        """
+        L = (self.length_stage + SI.c*self.delay_per_stage - self.l_gap/2 - self.l_kick/2 - self.l_diag)/4
+
+        return L
+    
+    def get_dipole_fields(self):
+        from abel.utilities.beam_physics import evolve_dispersion, evolve_R56
+
+        p = self.E_nom/SI.c # eV/c, also: beam rigidity 
+
+        ### Create constraints (delay and length of lattice). The function returns 2 values that should be kept around 0 while optimizing ###
+        def constraint(Bs):
+            """
+            Bs= [B1, B2, B3], the B-fields to be optimized. Same input as in the optimize-function
+            """
+            B4 = -(Bs[0] + Bs[1] + Bs[2]) # Write the signs implicitly
+
+            inv_r1 = Bs[0]/p
+            inv_r2 = Bs[1]/p
+            inv_r3 = Bs[2]/p
+            inv_r4 = B4/p
+
+            L_dipoles = self.get_dipole_lengths()
+
+            # Calculate the bend angles
+            theta1, theta2, theta3, theta4 = L_dipoles*inv_r1, L_dipoles*inv_r2, L_dipoles*inv_r3, L_dipoles*inv_r4
+
+            # first dipole:
+            if theta1==0:
+                const1 = L_dipoles
+            else:
+                const1 = np.sin(abs(theta1))/inv_r1
+            # second dipole
+            if theta2==0:
+                const2 = L_dipoles
+            else:
+                const2 = np.sin(abs(theta2/2))/inv_r2
+            # third dipole
+            if theta3==0:
+                const3 = L_dipoles
+            else:
+                const3 = np.sin(abs(theta3/2))/inv_r3
+            # fourth dipole
+            if theta4==0:
+                const4 = L_dipoles
+            else:
+                const4 = np.sin(abs(theta4))/inv_r4
+
+            # calculate projected length and subtract L_stage to make the constraint 0
+            constraint1 = self.l_kick/2 + const1 + 2*const2*np.cos(theta1+theta2/2) + \
+                    self.l_diag*np.cos(theta1 + theta2) + 2*const3*np.cos(theta1+theta2+theta3/2) + \
+                        const4 + self.l_gap/2 - self.length_stage # this must be 0 for our lattice to be valid (following the stages w. interstages)
+            """
+            With these constraints we can optimize for ls (see above) to make the dispersion prime and R56 zero.
+            The constraints ensure the lengths give the desired delay, and makes sure the longitudinal length (projected length) is the same as the length of the stage.
+            """
+            return constraint1
         
-        from scipy.optimize import root_scalar
-        res = root_scalar(func, x0=0.5)
-        print("B-field calculation converged: ", res.converged)
-        print("B-field: ", res.root)
-        return res.root
+        constraint_scipy = NonlinearConstraint(constraint, lb=-1e-9, ub=1e-9) #Making the constraint with the bounds around 0
+
+        ### Create initial guess that conforms to this constraint (setting all lengths equal) ###
+        x0 = self.x0
+
+        def get_L_proj(x, B):
+            # 1 B-value, positive
+            r13 = p/B
+            r24 = p/(B/x)
+            L = self.get_dipole_lengths()
+
+            theta1, theta2, theta3, theta4 = L/r13, -L/r24, -L/r13, L/r24
+
+            L_proj = self.l_kick/2 + r13*np.sin(abs(theta1)) + 2*r24*np.sin(abs(theta2/2))*np.cos(theta1+theta2/2) + \
+                            self.l_diag*np.cos(theta1 + theta2) + 2*r13*np.sin(abs(theta3)/2)*np.cos(theta1+theta2+theta3/2) + \
+                                r24*np.sin(abs(theta4)) + self.l_gap/2
+            return L_proj
+
+        # Find B-field to make x = length_fraction
+        B_calc = lambda B: get_L_proj(x0, B) - self.length_stage
+
+        opt_dipole = root_scalar(B_calc, x0=self.B_dipole)
+
+        B0 = [opt_dipole.root, -opt_dipole.root, -opt_dipole.root] # Initial guess in optimizing for Dispersion-prime and R56, manually set nr 2 and 3 negative
+        
+
+        def optimize_dipole_fields(Bs):
+            D0 = 0
+            Dp0 = 0
+
+            if not self.ks:
+                self.get_quad_strengths()
+
+            ls_list, inv_rs_list, ks_list = self.get_elements_arrays(Bs=Bs, ks=self.ks) # Get expanded numpy arrays
+
+            _, Dpx, _ = evolve_dispersion(ls=ls_list, ks=ks_list, inv_rhos=inv_rs_list)
+
+            R56, _ = evolve_R56(ls=ls_list, ks=ks_list, inv_rhos=inv_rs_list)
+
+            return Dpx**2 + (R56*1e2)**2
+        
+        B_bounds = [(-1.3,1.3)]*3 # Let B-values be negative
+
+        opt = minimize(optimize_dipole_fields, x0=B0, bounds=B_bounds, constraints=constraint_scipy, options={'maxiter': 1000})
+        print(opt)
+            
+        return opt.x
+    
+    def get_quad_strengths(self):
+        ### Optimize for alpha_x/y to get quad-strengths ###
+        # ls_B = [l_dipole1, l_dipole2, l_dipole3, l_diag, B1, B2]
+        from abel.utilities.beam_physics import evolve_beta_function
+        from scipy.optimize import minimize
+
+        beta0_x = 5
+        beta0_y = 5
+        ls_list, = self.get_elements_arrays() # Get expanded numpy array of lengths
+
+
+        def optimize_quads(ks_betas, plot=False):
+            """
+            Assume 0 bend while optimizing for the quads. This means the triplets are in the middle
+            ks_betas = [k1, k2, k3, betax, betay]
+            """
+            ls_list, ks_list = self.get_elements_arrays(ks=ks_betas[:3]) # Get expanded numpy arrays
+            if plot:
+                print(ls_list)
+                print(ks_list)
+
+
+            _, alpha_x, _ = evolve_beta_function(ls_list, ks_list, beta0=ks_betas[3], alpha0=0, plot=plot)
+            _, alpha_y, _ = evolve_beta_function(ls_list, ks_list, beta0=ks_betas[4], alpha0=0, plot=plot)
+
+            return alpha_x**2 + alpha_y**2
+        
+        f0_1 = 2 * ls_list[5]  # drift between focusing quads in triplet
+        f0_2 = np.sum(ls_list[-6:])*2 # distance from mid quad to next mid quad (ish)
+        f0_3 = f0_2
+        k0 = [1/f0_1/self.l_quads, -1/f0_2/self.l_quads, 1/f0_3/self.l_quads, beta0_x, beta0_y] # initial guess
+        k_bounds = [(0,50)]*3
+        k_bounds[1] = (-50,0)
+        k_bounds.extend([(0.1,100)]*2)
+
+        opt = minimize(optimize_quads, x0=k0, bounds=k_bounds)
+        print(opt)
+        #assert opt.fun<1e-3
+        self.ks = list(opt.x[:3]) # set the quad-strengths in the lattice
+        optimize_quads(opt.x, plot=True)
+        return opt.x[3:]
+
+    
+    def get_elements_arrays(self, Bs=None, ks=None):
+        # ls = [l_dipole1, l_dipole2, l_dipole3, l_diag, B1, B2]
+        p = self.E_nom/SI.c
+
+        L_dipoles = self.get_dipole_lengths()
+        L_to_first_quad = 0.1 #m
+        L_drift_diag = (self.l_diag - 3*self.l_quads - 2*L_to_first_quad)/2
+
+        ls_list = [self.l_kick/2, L_dipoles, L_dipoles, L_to_first_quad, self.l_quads, L_drift_diag, self.l_quads, L_drift_diag, self.l_quads, L_to_first_quad, L_dipoles, L_dipoles, self.l_gap/2]
+        assert abs(np.sum(ls_list) - (self.length_stage + self.delay_per_stage*SI.c)) < 1e-14
+        yield np.array(ls_list)
+
+        if Bs is not None:
+            assert len(Bs)==3
+            B4 = -(Bs[1] + Bs[2] + Bs[0]) # Explicitly using the signs of the field-values
+
+            inv_r1 = Bs[0]/p
+            inv_r2 = Bs[1]/p
+            inv_r3 = Bs[2]/p
+            inv_r4 = B4/p
+            inv_rs_list = [0, inv_r1, inv_r2, 0, 0, 0, 0, 0, 0, 0, inv_r3, inv_r4, 0]
+
+            yield np.array(inv_rs_list)
+            
+        if ks is not None:
+            assert len(ks)==3
+            ks_list = [0, 0, 0, 0, ks[0], 0, ks[1], 0, ks[2], 0, 0, 0, 0]
+            yield np.array(ks_list)
+
+
+
 
     def get_lattice_lengths(self, delay):
         pass #### To be implemented ####
@@ -136,22 +300,26 @@ class DriverDelaySystem(BeamDeliverySystem):
         # Start optimizing for dispersion and R56 under constraint of delay and length to hold
 
     def inv_r2phi(self, inv_rs: list):
-        phis=[]
-        for inv_r in inv_rs:
-            phi = self.l_dipole*inv_r
-            phis.append(phi)
-        return phis
+            pass
+        #phis=[]
+        #for inv_r in inv_rs:
+            #phi = self.l_dipole*inv_r
+            #phis.append(phi)
+            """
+            phis returned as radians
+            """
+        #return phis
     
     def phi2inv_r(self, phis: list):
         """
         phis given in rads
         """
-        inverse_rs = []
-        for phi in phis:
-            inv_r = phi/self.l_dipole
-            inverse_rs.append(inv_r)
-        return inverse_rs
-
+        #inverse_rs = []
+        #for phi in phis:
+        #    inv_r = phi/self.l_dipole
+        #    inverse_rs.append(inv_r)
+        #return inverse_rs
+        pass
     
     def get_lattice2stages(self, ks, return_lists=False):
         """
@@ -183,131 +351,6 @@ class DriverDelaySystem(BeamDeliverySystem):
         
     def get_lattice(self, ks):
         return self.get_lattice2stages(ks)*(self.num_stages//2)
-    
-    def match_quads(self):
-        from abel.utilities.beam_physics import evolve_dispersion, evolve_R56
-        # Initial guess
-        ff = min([self.l_straight, self.l_dipole])
-        if self.initial_focus_plane=="x":
-            ff=ff
-        elif self.initial_focus_plane=="y":
-            ff=-ff
-        else:
-            raise ValueError("Invalid choice of initial focus plane. Must be x or y (as a string).")
-        fd = -ff
-        kx0 = 1/ff/self.l_quads/2
-        ky0 = 1/fd/self.l_quads/2
-
-        x0 = [kx0, ky0] #### To be implemented ####
-            
-        def minimize_periodic(params):
-            _, ls, ks, phis = self.get_lattice2stages(params, return_lists=True) # phis given in degs, cause that's what the impactx lattice takes
-
-            phis = np.deg2rad(phis)
-            inverse_rs = self.phi2inv_r(phis)
-            """
-            Match dispersion and R56.
-            params = [k1, k2, ...]
-            """
-
-            D, _, _ = evolve_dispersion(np.array(ls), np.array(inverse_rs), np.array(ks), Dx0=0, Dpx0=0, fast=True, plot=False, high_res=False)
-
-            R56, _ = evolve_R56(np.array(ls), np.array(inverse_rs), np.array(ks), Dx0=0, Dpx0=0, fast=True, plot=False, high_res=False)
-
-            # D´ is unitless, R56 is m/E. Normalise with energy per length
-            R56_norm = self.E_nom/1e8 / self.cell_length
-            #print(D**2, "  ", (R56*R56_norm)**2)
-            return (R56*R56_norm)**2 + (D/self.cell_length)**2 #+ Dp**2
-        
-
-        res = minimize(minimize_periodic, x0=x0)
-        print(res)
-        self.ks = res.x
-    
-    def match_betas(self):
-        """
-        Use the quad-values found earlier, to find periodic beta/alpha-function
-        """
-        if not list(self.ks):
-            self.match_quads()
-
-        from abel.utilities.beam_physics import evolve_beta_function
-        # Initial guess
-        if self.initial_focus_plane=="x":
-            beta0 = [10, 20] # betas
-        else:
-            beta0 = [20, 10] # betas
-
-        def minimize_periodic(params):
-            """
-            params = [beta_x, beta_y]
-            """
-            _, ls, ks, _ = self.get_lattice2stages(self.ks, return_lists=True)                
-
-            beta_x, alpha_x, _ = evolve_beta_function(ls, np.array(ks), beta0=params[0], alpha0=0, fast=True)
-            beta_y, alpha_y, _ = evolve_beta_function(ls, -np.array(ks), beta0=params[1], alpha0=0, fast=True)
-
-            return (beta_x-params[1])**2/self.cell_length**2 + (beta_y-params[0])**2/self.cell_length**2 + alpha_x**2 + alpha_y**2
-        
-
-        res = minimize(minimize_periodic, x0=beta0, bounds=[(1e-5, 100)]*2)
-
-        print(res)
-        betas = res.x
-        return betas
-    
-    def match_quads_and_betas(self):
-        from abel.utilities.beam_physics import evolve_beta_function, evolve_dispersion, evolve_R56, evolve_second_order_dispersion
-
-        ff = self.l_straight
-
-        if self.initial_focus_plane=="x":
-            ff=ff
-            init = [10, 20] # betas
-        else:
-            ff=-ff
-            init = [20, 10] # betas
-
-        fd = -ff
-        kx0 = 1/ff/self.l_quads/2
-        ky0 = 1/fd/self.l_quads/2
-
-        #### To be implemented ####
-        # init.extend(ks)
-
-        def minimize_periodic(params):
-            beta_x0 = params[0]
-            beta_y0 = params[1]
-
-            ks = params[2:]
-
-            _, ls, ks, phis = self.get_lattice2stages(ks=ks, return_lists=True) # phis given in degs
-
-            phis = np.deg2rad(phis)
-
-            inverse_rs = self.phi2inv_r(phis)
-
-            D, _, _ = evolve_dispersion(np.array(ls), np.array(inverse_rs), np.array(ks), Dx0=0, Dpx0=0, fast=True, plot=False, high_res=False)
-            #DD, _, _ = evolve_second_order_dispersion(np.array(ls), inv_rhos=np.array(inverse_rs), ks=np.array(ks),\
-            #                                            ms=ms, taus=taus, fast=True)
-            R56, _ = evolve_R56(np.array(ls), np.array(inverse_rs), np.array(ks), Dx0=0, Dpx0=0, fast=True, plot=False, high_res=False)
-
-            # D´ is unitless, R56 is m/E. Normalise with energy per length
-            R56_norm = self.E_nom/1e7 / self.cell_length
-
-            beta_x, alpha_x, _ = evolve_beta_function(ls, np.array(ks), beta0=beta_x0, alpha0=0, fast=True)
-            beta_y, alpha_y, _ = evolve_beta_function(ls, -np.array(ks), beta0=beta_y0, alpha0=0, fast=True)
-
-            Dp_weigth = 1
-            #(Dp*Dp_weigth)**2 + (DD/self.cell_length)**2 + (D/self.cell_length)**2 + (DD/self.cell_length)**2 +
-            return (R56*R56_norm)**2  + (D/self.cell_length)**2 + alpha_y**2 + alpha_x**2 + \
-                (beta_x-beta_x0)**2/self.cell_length**2 + (beta_y-beta_y0)**2/self.cell_length**2
-        
-
-        res = minimize(minimize_periodic, x0 = [], options={"maxiter": 5000})
-        print(res)
-        self.ks = res.x[2:]
-        return res.x[0], res.x[1]
 
     
     def track(self, beam0, savedepth=0, runnable=None, verbose=False):
