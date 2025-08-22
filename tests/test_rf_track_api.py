f1c5da50b88c57cd7aa64b4771e116bcaaa8bfdb
Ben Chen <ben_chen93@hotmail.com>
2025-08-22 22:45:42 +0200

    Added tests/test_rf_track_api.py and added tests/test_rf_track_api.py::test_abel_beam2rft_beam.

---
+++ tests/test_rf_track_api.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+ABEL : unit tests for the RF-Track API
+=======================================
+
+This file is a part of ABEL.
+Copyright 2022– C.A.Lindstrøm, J.B.B.Chen, O.G.Finnerud,
+D.Kallvik, E.Hørlyk, K.N.Sjobak, E.Adli, University of Oslo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import pytest
+import numpy as np
+import scipy.constants as SI
+from abel.classes.source.impl.source_basic import SourceBasic
+from abel.classes.beam import Beam
+
+def setup_basic_driver_source():
+    driver = SourceBasic()
+    driver.bunch_length = 42e-6                                                   # [m] This value is for trapezoid.
+    driver.z_offset = 300e-6                                                      # [m]
+
+    driver.num_particles = 100000                                                 
+    driver.charge = -2.7e10 * SI.e                                                # [C]
+    driver.energy = 31.25e9                                                       # [eV]
+    driver.rel_energy_spread = 0.01                                               # Relative rms energy spread
+
+    driver.emit_nx, driver.emit_ny = 50e-6, 100e-6                                # [m rad]
+    driver.beta_x, driver.beta_y = 0.5, 0.5                                       # [m]
+
+    driver.symmetrize = True
+
+    return driver
+
+
+def setup_basic_main_source(plasma_density=6.0e20, ramp_beta_mag=5.0):
+    from abel.utilities.plasma_physics import beta_matched
+
+    main = SourceBasic()
+    main.bunch_length = 40.0e-06                                                  # [m], rms.
+    main.num_particles = 10000                                               
+    main.charge = -SI.e * 1.0e10                                                  # [C]
+
+    # Energy parameters
+    main.energy = 3.0e9                                                           # [eV]
+    main.rel_energy_spread = 0.02                                                 # Relative rms energy spread
+
+    # Emittances
+    main.emit_nx, main.emit_ny = 15e-6, 0.1e-6                                    # [m rad]
+
+    # Beta functions
+    main.beta_x = beta_matched(plasma_density, main.energy) * ramp_beta_mag       # [m]
+    main.beta_y = main.beta_x                                                     # [m]
+
+    # Offsets
+    main.z_offset = 0.00e-6                                                       # [m]
+
+    # Other
+    main.symmetrize_6d = True
+
+    return main
+
+
+@pytest.mark.linac_unit_test
+def test_abel_beam2rft_beam():
+    """
+    Tests for ``abel_beam2rft_beam()``.
+    """
+
+    from abel.apis.rf_track.rf_track_api import abel_beam2rft_beam
+
+    source = setup_basic_main_source()
+    beam = source.track()
+    beam_rft = abel_beam2rft_beam(beam)
+
+
+    # ========== Single beam, homogeneous charge ==========
+    # Get the RF-Track beam phase space using functions from RF-Track
+    phase_space_rft = beam_rft.get_phase_space()  # RFT units and in the format [ X Px Y Py Z Pz ]
+
+    assert np.allclose(phase_space_rft[:,0], beam.xs()*1e3, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,1], beam.pxs()*SI.c/SI.e/1e6, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,2], beam.ys()*1e3, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,3], beam.pys()*SI.c/SI.e/1e6, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,4], beam.zs()*1e3, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,5], beam.pzs()*SI.c/SI.e/1e6, rtol=1e-15, atol=0.0)
+
+
+    # ========== Slightly enlarge the transverse region by adding chargeless ghost particles ==========
+    x_min = beam.xs().min() + np.sign(beam.xs().min())*1e-6
+    x_max = beam.xs().max() + np.sign(beam.xs().max())*1e-6
+    y_min = beam.ys().min() + np.sign(beam.ys().min())*1e-6
+    y_max = beam.ys().max() + np.sign(beam.ys().max())*1e-6
+    z_end = beam.zs().min()
+    z_start = beam.zs().max()
+    
+    X, Y, Z = np.meshgrid([x_min, x_max], [y_min, y_max], [z_start, z_end], indexing='ij')
+    empty_beam = Beam()
+    empty_beam.set_phase_space(Q=0,
+                               xs=X.flatten(),
+                               ys=Y.flatten(),
+                               zs=Z.flatten(), 
+                               uxs=np.ones_like(X.flatten())*beam.uxs()[0],
+                               uys=np.ones_like(X.flatten())*beam.uys()[0],
+                               uzs=np.ones_like(X.flatten())*beam.uzs()[0],
+                               weightings=np.ones_like(X.flatten())*beam.weightings()[0],
+                               particle_mass=beam.particle_mass)
+    
+    comb_beam = beam + empty_beam
+    comb_beam.particle_mass = beam.particle_mass
+
+    assert len(empty_beam) == 8
+
+    comb_beam_rft = abel_beam2rft_beam(comb_beam)
+
+    phase_space_rft = comb_beam_rft.get_phase_space()  # RFT units and in the format [ X Px Y Py Z Pz ]
+
+    assert np.allclose(phase_space_rft[:,0], comb_beam.xs()*1e3, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,1], comb_beam.pxs()*SI.c/SI.e/1e6, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,2], comb_beam.ys()*1e3, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,3], comb_beam.pys()*SI.c/SI.e/1e6, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,4], comb_beam.zs()*1e3, rtol=1e-15, atol=0.0)
+    assert np.allclose(phase_space_rft[:,5], comb_beam.pzs()*SI.c/SI.e/1e6, rtol=1e-15, atol=0.0)
+    assert phase_space_rft.shape[0] == len(empty_beam) + len(beam)
+    #assert np.isclose(, beam.charge(), rtol=1e-15, atol=0.0)
+    #assert np.isclose(, beam.particle_mass, rtol=1e-15, atol=0.0)
+    #assert np.isclose(, beam.weightings(), rtol=1e-15, atol=0.0)
+    assert np.isclose(phase_space_rft[:,0].max(), x_max*1e3, rtol=1e-15, atol=0.0)
+    assert np.isclose(phase_space_rft[:,0].min(), x_min*1e3, rtol=1e-15, atol=0.0)
+    assert np.isclose(phase_space_rft[:,2].max(), y_max*1e3, rtol=1e-15, atol=0.0)
+    assert np.isclose(phase_space_rft[:,2].min(), y_min*1e3, rtol=1e-15, atol=0.0)
+    assert np.isclose(phase_space_rft[:,4].max(), z_start*1e3, rtol=1e-15, atol=0.0)
+    assert np.isclose(phase_space_rft[:,4].min(), z_end*1e3, rtol=1e-15, atol=0.0)
+
+
+    # ========== Test for non-homogeneous beam charge ==========
+
+    #comb_beam_rft = abel_beam2rft_beam(comb_beam, homogen_beam_charge=False)
+    
+
