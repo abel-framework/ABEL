4a3f8ee694e776537feef648545c7f43bcc24422
Kyrre Sjobak <kyrsjo@gmail.com>
2025-12-15 16:37:29 +0100

    Kill only test_StageReducedModels.py::test_stage_length_gradient_energyGain

--- tests/test_StageReducedModels.py
+++ tests/test_StageReducedModels.py
@@ -113,64 +113,6 @@ def setup_StageReducedModels(driver_source, main_source, plasma_density=6.0e20, 
 
 
 @pytest.mark.StageReducedModels
-def test_stage_length_gradient_energyGain():
-    """
-    Tests ensuring that the flattop length and total length of the stage as well 
-    as nominal gradient and nominal energy gain are set correctly.
-    """
-
-    np.random.seed(42)
-
-    plasma_density = 6.0e+20                                                      # [m^-3]
-    ramp_beta_mag = 5.0
-    enable_xy_jitter = False
-    enable_xpyp_jitter = False
-    enable_tr_instability = False
-    enable_radiation_reaction = False
-    enable_ion_motion = False
-    use_ramps = True
-    drive_beam_update_period = 0
-
-    driver_source = setup_trapezoid_driver_source(enable_xy_jitter, enable_xpyp_jitter)
-    main_source = setup_basic_main_source(plasma_density, ramp_beta_mag)
-
-
-    # ========== Set stage length and nominal energy gain ==========
-    stage = setup_StageReducedModels(driver_source, main_source, plasma_density, ramp_beta_mag, enable_tr_instability, enable_radiation_reaction, enable_ion_motion, use_ramps, drive_beam_update_period, return_tracked_driver=False, store_beams_for_tests=False)
-
-    stage.length_flattop = 7.8                                                    # [m]
-    stage.nom_energy_gain = 7.8e9                                                 # [eV]
-
-    linac = PlasmaLinac(source=main_source, stage=stage, num_stages=1)
-    linac.run('test_stage_length_gradient_energyGain', overwrite=True, verbose=False)
-
-    assert np.allclose(stage.nom_energy_gain_flattop, 7.8e9, rtol=1e-15, atol=0.0)
-    assert np.allclose(stage.nom_energy_gain, 7.8e9, rtol=1e-15, atol=0.0)
-    assert np.allclose(stage.length_flattop, 7.8, rtol=1e-15, atol=0.0)
-    assert np.allclose(stage.nom_accel_gradient_flattop, 1.0e9, rtol=1e-15, atol=0.0)
-    assert np.allclose(stage.length, stage.length_flattop + stage.upramp.length_flattop + stage.downramp.length_flattop, rtol=1e-15, atol=0.0)
-
-
-    # ========== Set nominal energy gain and flattop nominal acceleration gradient ==========
-    stage = setup_StageReducedModels(driver_source, main_source, plasma_density, ramp_beta_mag, enable_tr_instability, enable_radiation_reaction, enable_ion_motion, use_ramps, drive_beam_update_period, return_tracked_driver=False, store_beams_for_tests=False, length_flattop=None)
-
-    stage.nom_energy_gain = 7.8e9                                                 # [eV]
-    stage.nom_energy_gain_flattop = 7.8e9                                         # [eV]
-    stage.nom_accel_gradient_flattop = 1.0e9                                      # [V/m]
-
-    linac = PlasmaLinac(source=main_source, stage=stage, num_stages=1)
-    linac.run('test_stage_length_gradient_energyGain', overwrite=True, verbose=False)
-    assert np.allclose(stage.nom_energy_gain_flattop, 7.8e9, rtol=1e-15, atol=0.0)
-    assert np.allclose(stage.nom_energy_gain, 7.8e9, rtol=1e-15, atol=0.0)
-    assert np.allclose(stage.length_flattop, 7.8, rtol=1e-15, atol=0.0)
-    assert np.allclose(stage.nom_accel_gradient_flattop, 1.0e9, rtol=1e-15, atol=0.0)
-    assert np.allclose(stage.length, stage.length_flattop + stage.upramp.length_flattop + stage.downramp.length_flattop, rtol=1e-15, atol=0.0)
-
-    # Remove output directory
-    shutil.rmtree(linac.run_path())
-
-
-@pytest.mark.StageReducedModels
 def test_driver_unrotation():
     """
     Tests for checking the driver being correctly un-rotated back to its 
@@ -499,3 +441,120 @@ def test_matched_beta_function():
     beta_mat_ramped = np.sqrt(2*energy2gamma(main_source.energy))/kp * 4.0
     assert np.allclose(stage_ramped.matched_beta_function(main_source.energy, match_entrance=False), beta_mat_ramped, rtol=1e-15, atol=0.0)
 
+
+@pytest.mark.StageReducedModels
+def test_trim_attr_reduce_pickle_size():
+    """
+    Tests for ``StageReducedModels.trim_attr_reduce_pickle_size()``.
+    """
+
+    np.random.seed(42)
+
+    driver_source = setup_trapezoid_driver_source()
+    main_source = setup_basic_main_source()
+    stage = setup_StageReducedModels(driver_source, main_source, use_ramps=True)
+
+    # Before calling StageReducedModels.trim_attr_reduce_pickle_size()
+    assert stage.upramp is not None
+    assert stage.upramp.initial is not None
+    assert stage.upramp.final is not None
+    assert stage.downramp is not None
+    assert stage.downramp.initial is not None
+    assert stage.downramp.final is not None
+
+    stage.trim_attr_reduce_pickle_size()
+
+    # After calling StageReducedModels.trim_attr_reduce_pickle_size()
+    assert stage.upramp.drive_beam is None
+    assert stage.upramp.initial is None
+    assert stage.upramp.final is None
+    assert stage.downramp.drive_beam is None
+    assert stage.downramp.initial is None
+    assert stage.downramp.final is None
+
+
+@pytest.mark.StageReducedModels
+def test_plotting_methods():
+    """
+    Tests for various ``StageReducedModels`` plotting methods.
+    """
+
+    import matplotlib.pyplot as plt
+    np.random.seed(42)
+
+    driver_source = setup_trapezoid_driver_source()
+    main_source = setup_basic_main_source()
+    stage = setup_StageReducedModels(driver_source, main_source, use_ramps=True, length_flattop=1.0)
+    stage.save_final_step = True
+
+    linac = PlasmaLinac(source=main_source, stage=stage, num_stages=1)
+    linac.run('test_plotting_methods', overwrite=True, verbose=False)
+    beam = linac.get_beam(-1)
+
+    linac.stages[-1].plot_wakefield(saveToFile=None, includeWakeRadius=False)
+    linac.stages[-1].plot_wakefield(saveToFile=None, includeWakeRadius=True)
+    linac.stages[-1].plot_wake(show_Ez=False, trace_rb=False, savefig=None, aspect='auto')
+    linac.stages[-1].plot_wake(show_Ez=True, trace_rb=True, savefig=None, aspect='auto')
+    linac.stages[-1].plot_Ez_rb_cut()
+    linac.stages[-1].plot_flattop_evolution()
+
+    # Close all plots
+    plt.close('all')
+
+    # Remove output directory
+    shutil.rmtree(linac.run_path())
+
+
+@pytest.mark.StageReducedModels
+def test_print_stage_beam_summary():
+    """
+    Tests for ``StageReducedModels.print_stage_beam_summary()``.
+    """
+
+    np.random.seed(42)
+
+    driver_source = setup_trapezoid_driver_source()
+    main_source = setup_basic_main_source()
+    stage = setup_StageReducedModels(driver_source, main_source, use_ramps=True)
+
+    stage.print_stage_beam_summary(initial_main_beam=main_source.track(), beam_out=main_source.track(), clean=True)
+
+
+@pytest.mark.StageReducedModels
+def test_print_summary():
+    """
+    Tests for ``StageReducedModels.print_summary()``.
+    """
+
+    np.random.seed(42)
+
+    driver_source = setup_trapezoid_driver_source()
+    main_source = setup_basic_main_source()
+    stage = setup_StageReducedModels(driver_source, main_source, use_ramps=True)
+
+    stage.print_summary()
+
+
+@pytest.mark.StageReducedModels
+def test_make_animations():
+    """
+    Tests for ``StageReducedModels`` functions for making animations.
+    """
+    from pathlib import Path
+    import os
+
+    np.random.seed(42)
+
+    driver_source = setup_trapezoid_driver_source()
+    main_source = setup_basic_main_source(energy=300.0e9)  # High energy for faster tracking.
+    stage = setup_StageReducedModels(driver_source, main_source, use_ramps=True)
+    stage.time_step_mod = 0.03                                                    # In units of betatron wavelengths/c.
+    stage.probe_evol_period = 18
+    stage.make_animations = True
+    stage.run_path = Path("tests/run_data")
+    os.makedirs(stage.run_path, exist_ok=True)
+
+    stage.nom_energy = main_source.energy                                         # [eV]
+    stage.track(main_source.track())
+
+    shutil.rmtree( os.path.join(stage.run_path, 'plots') )
