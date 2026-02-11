d5ff24377f5a3d962c6a508e9f4ad547909ee110
Ben Chen <6263622+ben-c-2013@users.noreply.github.com>
2026-02-11 15:13:24 +0100

    Removed StageBasic.driver_source getter.

--- abel/classes/stage/impl/stage_basic.py
+++ abel/classes/stage/impl/stage_basic.py
@@ -472,15 +472,13 @@ class StageBasic(Stage):
     
 
     # ==================================================
-    @property
-    def driver_source(self) -> Source | DriverComplex | None:
+    @Stage.driver_source.setter
+    def driver_source(self, source : Source | DriverComplex | None):
         """
         The driver source or the driver complex of the stage. The generated 
         drive beam's beam axis is always aligned to its propagation direction.
         """
-        return self._driver_source
-    @driver_source.setter
-    def driver_source(self, source : Source | DriverComplex | None):
+        
         # Delegate to parent setter
         Stage.driver_source.fset(self, source)
         
