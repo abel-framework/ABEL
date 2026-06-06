1292be2de64d4b1d71213685238f6180b24e4bfa
Carl A. Lindstrøm <c.a.lindstrom@fys.uio.no>
2026-06-06 10:27:06 +0200

    Fixed bug with naming of limit attribute in phase_space_density.

--- src/abel/classes/beam.py
+++ src/abel/classes/beam.py
@@ -1308,16 +1308,16 @@ class Beam():
     
     ## phase spaces
     
-    def phase_space_density(self, hfcn, vfcn, hbins=None, vbins=None, hlim=None, vlim=None):
+    def phase_space_density(self, hfcn, vfcn, hbins=None, vbins=None, hlims=None, vlims=None):
         self.remove_nans()
         if hbins is None:
             hbins = round(np.sqrt(len(self))/2)
-            if hlim is not None:
-                hbins = np.linspace(np.min(hlim), np.max(hlim), hbins)
+            if hlims is not None:
+                hbins = np.linspace(np.min(hlims), np.max(hlims), hbins)
         if vbins is None:
             vbins = round(np.sqrt(len(self))/2)
-            if vlim is not None:
-                vbins = np.linspace(np.min(vlim), np.max(vlim), vbins)
+            if vlims is not None:
+                vbins = np.linspace(np.min(vlims), np.max(vlims), vbins)
         counts, hedges, vedges = np.histogram2d(hfcn(), vfcn(), weights=self.qs(), bins=(hbins, vbins))
         hctrs = (hedges[0:-1] + hedges[1:])/2
         vctrs = (vedges[0:-1] + vedges[1:])/2
@@ -1331,10 +1331,10 @@ class Beam():
         return density, hctrs, vctrs
     
     def density_lps(self, zbins=None, Ebins=None, zlims=None, Elims=None):
-        return self.phase_space_density(self.zs, self.Es, hbins=zbins, vbins=Ebins, hlim=zlims, vlim=Elims)
+        return self.phase_space_density(self.zs, self.Es, hbins=zbins, vbins=Ebins, hlims=zlims, vlims=Elims)
     
     def density_transverse(self, hbins=None, vbins=None, xlims=None, ylims=None):
-        return self.phase_space_density(self.xs, self.ys, hbins=hbins, vbins=vbins, hlim=xlims, vlim=yElims)
+        return self.phase_space_density(self.xs, self.ys, hbins=hbins, vbins=vbins, hlims=xlims, vlims=ylims)
 
     
     # ==================================================
@@ -1709,7 +1709,7 @@ class Beam():
         cb.ax.set_ylabel('Charge density (pC/um/mrad)')
 
     def plot_transverse_profile(self, xlims=None, ylims=None):
-        dQdxdy, xs, ys = self.phase_space_density(self.xs, self.ys, xlims=xlims, ylims=ylims)
+        dQdxdy, xs, ys = self.phase_space_density(self.xs, self.ys, hlims=xlims, vlims=ylims)
 
         fig, ax = plt.subplots()
         fig.set_figwidth(8)
