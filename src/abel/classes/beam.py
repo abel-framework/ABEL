e57df036c80846301e9563eaca7152a1e05ffebc
Carl A. Lindstrøm <c.a.lindstrom@fys.uio.no>
2026-06-07 19:43:38 +0200

    Reverted updated attribute name in Beam.load for backward compatibility.

--- src/abel/classes/beam.py
+++ src/abel/classes/beam.py
@@ -2074,13 +2074,11 @@ class Beam():
         
     # load beam (from OpenPMD format)
     @classmethod
-    def load(_, filename0, beam_name='beam'):
+    def load(_, filename, beam_name='beam'):
 
         # resolve if alias
-        if os.path.islink(filename0):
-            filename = os.readlink(filename0)
-        else:
-            filename = filename0
+        if os.path.islink(filename):
+            filename = os.readlink(filename)
         
         # load file
         series = io.Series(filename, io.Access.read_only)
